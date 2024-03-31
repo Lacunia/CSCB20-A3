a4582966459066ab91e10635f116da1c47c68465
Lucy Lin <lucylin6699@gmail.com>
2024-03-30 22:55:25 -0400

    Add various routes and functions in app.py and created base.html

--- app.py
+++ app.py
@@ -11,65 +11,59 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///notes.db' #this set up the pa
 app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes = 10) # keep the session on for 10 min
 db = SQLAlchemy(app) 
 
-# when accessing from the terminal, use the following to manually access the database:
-# >>> .venv (activate it)
-# >>> py
-# >>> from app import app, db
-# >>> app.app_context().push()
-# >>> db.create_all()
-# >>> from app import Person, Notes
-# >>> person3 = Person(username='Student2', email='student2@email.com', password='password2')
-# >>> person4 = Person(username='Student3', email='student3@email.com', password='password3')
-# >>> db.session.add(person3)
-# >>> db.session.add(person4)
-# >>> db.session.commit()
-
-# >>> Person.query.all()  <-- return the result in a list
-# [Person('Purva', 'purva@gmail.com'), Person('Student', 'student@gmail.com'), Person('Student2', 'student2@email.com')
-# Person('Student3', 'student3@email.com')]
-
-# >>> Person.query.first() 
-# Person('Purva', 'purva@gmail.com')
-
-# >>> Person.qeury.filter_by(username='Purva').all()   <-- find all data with username 'Purva'
-# [Person('Purva', 'purva@gmail.com') ]
-# >>> Person.qeury.filter_by(username='Purva').first()   <-- find the first data with username 'Purva'
-# Person('Purva', 'purva@gmail.com')
-
-# >>> P1 = Person.qeury.filter_by(username='Purva').first()   <-- store the first data with username 'Purva' into P1
-# >>> P1.id
-# 1
-# >>> P1.password
-# 'password'
 
 class Person(db.Model): # create a table using sqlalchemy
     __tablename__ = 'Person'
-    id = db.Column(db.Integer, primary_key=True) 
-    username = db.Column(db.String(20), unique=True, nullable=False)
-    email=db.Column(db.String(30), unique=True, nullable=False)
+    utorid = db.Column(db.String(20), primary_key=True) 
+    name = db.Column(db.String(20), nullable=False)
+    email = db.Column(db.String(30), unique=True, nullable=False)
     password = db.Column(db.String(20), nullable=False)
-    notes=db.relationship('Notes', backref='author', lazy=True)  # not creating another column, just a relationship
+    role = db.Column(db.String(1), nullable=False)
+    grades = db.relationship('Grades', backref='person', lazy=True, uselist=False) # ensures one-to-one relationship
+
+    def __init__(self, utorid, name, email, password, role, **kwargs):
+        super().__init__(utorid=utorid, name=name, email=email, password=password, role=role, **kwargs)
+        self.grades = Grades(utorid=utorid)
 
     def __repr__(self): # string representation of the table
-        return f"Person('{self.username}', '{self.email}')"
+        return f"Person('{self.utorid}', '{self.name}', '{self.role})"
     
-class Notes(db.Model): # create a table using sqlalchemy
-    __tablename__ = 'Notes'
-    id = db.Column(db.Integer, primary_key=True) 
-    title = db.Column(db.String(120), nullable=False)
-    date_posted=db.Column(db.DateTime, nullable=False, default=datetime.now) # add the current date
-    content = db.Column(db.Text, nullable=False)
-    person_id = db.Column(db.Integer, db.ForeignKey('Person.id'), nullable=False)
+class Grades(db.Model): # create a table using sqlalchemy
+    __tablename__ = 'Grades'
+    id = db.Column(db.Integer, primary_key=True)
+    utorid = db.Column(db.String(20), db.ForeignKey('Person.utorid'), nullable=False) 
+    assignment_1 = db.Column(db.Float)
+    assignment_2 =db.Column(db.Float)
+    assignment_3  = db.Column(db.Float)
+    midterm = db.Column(db.Float)
+    final = db.Column(db.Float)
 
     def __repr__(self):
-        return f"Post('{self.title}', '{self.date_posted}')"
+        return f"Post('{self.utorid}')"
 
 
 @app.route('/') # the function will be executed if the web route has '/' or '/home' as extensions
-@app.route('/home')
-def home():
-    pagename = 'Home'
-    return render_template('home.html', pagename = pagename)  # render_template actually linking to the html
+@app.route('/index')
+def index():
+    # if 'user_id' not in session:
+    #     return redirect(url_for('login'))
+    
+    # # Retrieve user's role
+    # user_id = session.get('user_id')
+    # user = db_session.query(User).filter(User.id == user_id).first()
+
+    # # Check user's role and render different templates accordingly
+    # if user:
+    #     if user.role == 'admin':
+    #         return render_template('index_admin.html')
+    #     elif user.role == 'user':
+    #         return render_template('index_user.html')
+    #     else:
+    #         return render_template('index_unknown.html')  # Handle unknown role
+    # else:
+    #     return render_template('index_unknown.html')  # Handle user not found
+    
+    return render_template('index.html')  # render_template actually linking to the html
     # we take the pagename and use it in the html
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -77,15 +71,19 @@ def register():
     if request.method == 'GET':
         return render_template('register.html')
     else:
-        user_name=request.form['Username']
+        utorid=request.form['Utorid']
+        name=request.form['Name']
         email=request.form['Email']
+        role=request.form['Role']
         hashed_password=bcrypt.generate_password_hash(request.form['Password']).decode('utf-8')
         # for added security when using bcrypt
 
         reg_details = (
-            user_name,
+            utorid,
+            name,
             email,
-            hashed_password
+            hashed_password,
+            role
         )
         add_users(reg_details)
         flash('Registration successful! Please login now.')
@@ -96,60 +94,90 @@ def login():
     if request.method=='GET':
         if 'name' in session:
             flash('Your are already logged in!')
-            return redirect(url_for('home'))
+            return redirect(url_for('index'))
         else:
             return render_template('login.html')
     else:
-        username=request.form['Username']
+        utorid=request.form['Utorid']
         password=request.form['Password']
-        person = Person.query.filter_by(username = username).first()
+        person = Person.query.filter_by(utorid = utorid).first()
         if not person or not bcrypt.check_password(person.password, password):
             flash('Please check your login details and try again', 'error')
             return render_template('login.html')
         else:
-            log_details=(
-                username,
-                password
-            )
-            session['name']=username
+            # log_details=(
+            #     utorid,
+            #     password
+            # )
+            session['user_id'] = person.utorid
             session.permanent=True
             flash('Logged in Successfully!')
-            return redirect(url_for('home'))
+            return redirect(url_for('index'))
 
-@app.route('/notes')
-def notes():
-    pagename = 'Lecture Notes'
-    query_notes_result = query_notes()
-    return render_template('notes.html', pagename = pagename, query_notes_result=query_notes_result) 
+@app.route('/calender')
+def calender():
+    return render_template('calender.html')
 
-@app.route('/add', methods = ['GET', 'POST']) # methods allow us to do something with the input
-def add():
+@app.route('/CourseTeam')
+def CourseTeam():
+    return render_template('CourseTeam.html')
+
+@app.route('/lecture')
+def lecture():
+    return render_template('lecture.html')
+
+@app.route('/lab')
+def lab():
+    return render_template('lab.html')
+
+@app.route('/assignment')
+def assignment():
+    return render_template('assignment.html')
+
+@app.route('/resources')
+def resources():
+    return render_template('resources.html')
+
+@app.route('/feedback')
+def feedback():
+    return render_template('feedback.html')
+
+@app.route('/grades')
+def grades():
+    return render_template('grades.html')
+# def notes():
+#     query_notes_result = query_notes()
+#     return render_template('notes.html', query_notes_result=query_notes_result) 
+
+@app.route('/manage', methods = ['GET', 'POST']) # methods allow us to do something with the input
+# helps instructors change/add a student's grade for an assignment 
+def manage():
     if request.method == 'GET':
-        pagename = 'Add Notes'
-        return render_template('add.html', pagename=pagename)
+        return render_template('manage.html')
     else: # render this if the method is POST
-        pagename = 'Success'
-        note_details = (
-            request.form['Note_ID'],
-            request.form['Title'],
-            request.form['Content'],
-            request.form['Your_ID']
+        grade_details = (
+            request.form['Utorid'],
+            request.form['Assignment'],
+            request.form['Grade']
         )
-        add_notes(note_details)
-        return render_template('add_success.html', pagename=pagename)
+        add_grades(grade_details)
+        flash("Student's grade changed successfully!")
+        return render_template('manage.html')
 
-# helps add the notes that we got from the form into the database    
-def add_notes(note_details):
-    note = Notes(id=note_details[0], title=note_details[1], content=note_details[2], person_id=note_details[3])
-    db.session.add(note)
+# change a student's grade for a specific assignment (into the db)    
+def add_grades(grade_details):
+    student = Grades.query.filter_by(utorid = grade_details[0])
+    setattr(student, grade_details[1], grade_details[2])
     db.session.commit()
 
-def query_notes():
-    query_notes = Notes.query.all()
-    return query_notes
+# get a student's grades (so we can display it in a table)
+def query_grades(utorid):
+    query_grade = Grades.query.filter_by(utorid = utorid).first()
+    return query_grade
 
+# add the register user into our db
 def add_users(reg_details):
-    user = Person(username=reg_details[0], email=reg_details[1], password=reg_details[2])
+    user = Person(utorid=reg_details[0], name=reg_details[1], email=reg_details[2], password=reg_details[3], role=reg_details[4])
     db.session.add(user)
     db.session.commit()
 
