48f3586f704d7a2b96ea3df7a302e190c1d96656
Lucy Lin <lucylin6699@gmail.com>
2024-04-04 15:56:37 -0400

    fixed login issue and changed navbar to display role specific tabs

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template, request, flash, redirect, url_for, session
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime, timedelta
-from flask_bcrypt import Bcrypt
+from flask_bcrypt import Bcrypt, check_password_hash
 
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
 
 app.config['SECRET_KEY'] = '\x9f\x83=0\xecRVY\xe02\x99p\xc6<\x8b\x18'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///notes.db' #this set up the path to our database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///login.db' #this set up the path to our database
 app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes = 10) # keep the session on for 10 min
 db = SQLAlchemy(app) 
 
@@ -47,23 +47,8 @@ class Grades(db.Model): # create a table using sqlalchemy
 def index():
     # if 'user_id' not in session:
     #     return redirect(url_for('login'))
-    
-    # # Retrieve user's role
-    # user_id = session.get('user_id')
-    # user = db_session.query(User).filter(User.id == user_id).first()
-
-    # # Check user's role and render different templates accordingly
-    # if user:
-    #     if user.role == 'admin':
-    #         return render_template('index_admin.html')
-    #     elif user.role == 'user':
-    #         return render_template('index_user.html')
-    #     else:
-    #         return render_template('index_unknown.html')  # Handle unknown role
-    # else:
-    #     return render_template('index_unknown.html')  # Handle user not found
-    
-    return render_template('index.html')  # render_template actually linking to the html
+    role = get_role()
+    return render_template('index.html', role=role)  # render_template actually linking to the html
     # we take the pagename and use it in the html
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -71,11 +56,11 @@ def register():
     if request.method == 'GET':
         return render_template('register.html')
     else:
-        utorid=request.form['Utorid']
-        name=request.form['Name']
-        email=request.form['Email']
-        role=request.form['Role']
-        hashed_password=bcrypt.generate_password_hash(request.form['Password']).decode('utf-8')
+        utorid=request.form['utorid']
+        name=request.form['name']
+        email=request.form['email']
+        role=request.form['role']
+        hashed_password=bcrypt.generate_password_hash(request.form['password']).decode('utf-8')
         # for added security when using bcrypt
 
         reg_details = (
@@ -89,77 +74,102 @@ def register():
         flash('Registration successful! Please login now.')
         return redirect(url_for('login')) # redirect from the current page to this page
 
+# add the register user into our db
+def add_users(reg_details):
+    user = Person(utorid=reg_details[0], name=reg_details[1], email=reg_details[2], password=reg_details[3], role=reg_details[4])
+    db.session.add(user)
+    db.session.commit()
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method=='GET':
-        if 'user_id' in session:
+        if 'user' in session:
             flash('You are already logged in!')
             return redirect(url_for('index'))
         else:
             return render_template('login.html')
     else:
-        utorid=request.form['utorid']
+        name=request.form['name']
         password=request.form['password']
-        person = Person.query.filter_by(utorid = utorid).first()
-        if not person or not bcrypt.check_password(person.password, password):
+        person = Person.query.filter_by(name = name).first()
+        if not person or not bcrypt.check_password_hash(person.password, password):
             flash('Please check your login details and try again', 'error') #'error' is optional
-            return render_template('login.html', login_status=False)
+            return render_template('login.html')
         else:
             # log_details=(
             #     utorid,
             #     password
             # )
-            session['user_id'] = person.utorid
+            session['user'] = name
             session.permanent=True  # closing the browser will not log out
             flash('Logged in Successfully!')
-            return redirect(url_for('index'), login_status=True, role=person.role, name=person.name)
+            return redirect(url_for('index'))
 
 # unsure if the logout function is correct
 @app.route('/logout', methods=['GET', 'POST'])
 def logout():
-    session.pop('user_id', None)
-    return redirect(url_for('index'), login_status=False)
+    session.pop('user', None)
+    return redirect(url_for('index'))
 
 @app.route('/calendar')
 def calendar():
-    return render_template('calendar.html')
+    role = get_role()
+    return render_template('calendar.html', role=role)
 
 @app.route('/CourseTeam')
 def CourseTeam():
-    return render_template('CourseTeam.html')
+    role = get_role()
+    return render_template('CourseTeam.html', role=role)
 
 @app.route('/lecture')
 def lecture():
-    return render_template('lecture.html')
+    role = get_role()
+    return render_template('lecture.html', role=role)
 
 @app.route('/lab')
 def lab():
-    return render_template('lab.html')
+    role = get_role()
+    return render_template('lab.html', role=role)
 
 @app.route('/assignment')
 def assignment():
-    return render_template('assignment.html')
+    role = get_role()
+    return render_template('assignment.html', role=role)
 
 @app.route('/resources')
 def resources():
-    return render_template('resources.html')
+    role = get_role()
+    return render_template('resources.html', role=role)
 
 @app.route('/feedback')
 def feedback():
-    return render_template('feedback.html')
+    role = get_role()
+    return render_template('feedback.html', role=role)
 
 @app.route('/grades')
 def grades():
-    return render_template('grades.html')
-# def notes():
-#     query_notes_result = query_notes()
-#     return render_template('notes.html', query_notes_result=query_notes_result) 
+    grades_result = None
+
+    if "user" in session:
+        name = session.get("user")
+        user = Person.query.filter_by(name = name).first()
+        utorid = user.utorid
+        grades_result = query_grades(utorid)
+    
+    role = get_role()
+    return render_template('grades.html', grades = grades_result, role=role)
+
+# get a student's grades (so we can display it in a table)
+def query_grades(utorid):
+    query_grade = Grades.query.filter_by(utorid = utorid).first()
+    return query_grade 
 
 @app.route('/manage', methods = ['GET', 'POST']) # methods allow us to do something with the input
 # helps instructors change/add a student's grade for an assignment 
 def manage():
     if request.method == 'GET':
-        return render_template('manage.html')
+        role = get_role()
+        return render_template('manage.html', role=role)
     else: # render this if the method is POST
         grade_details = (
             request.form['Utorid'],
@@ -168,7 +178,8 @@ def manage():
         )
         add_grades(grade_details)
         flash("Student's grade changed successfully!")
-        return render_template('manage.html')
+        role = get_role()
+        return render_template('manage.html', role=role)
 
 # change a student's grade for a specific assignment (into the db)    
 def add_grades(grade_details):
@@ -176,16 +187,17 @@ def add_grades(grade_details):
     setattr(student, grade_details[1], grade_details[2])
     db.session.commit()
 
-# get a student's grades (so we can display it in a table)
-def query_grades(utorid):
-    query_grade = Grades.query.filter_by(utorid = utorid).first()
-    return query_grade
 
-# add the register user into our db
-def add_users(reg_details):
-    user = Person(utorid=reg_details[0], name=reg_details[1], email=reg_details[2], password=reg_details[3], role=reg_details[4])
-    db.session.add(user)
-    db.session.commit()
+# get the role of the user
+def get_role():
+    role = None
+
+    # # Retrieve user's role
+    if 'user' in session:
+        username = session.get('user')
+        user = Person.query.filter_by(name = username).first()
+        role = user.role
+    return role
 
 if __name__ == '__main__':
     app.run(debug=True)
