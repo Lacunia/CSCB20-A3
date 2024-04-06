1706e6952e6e12340b856ac53f4ed1f9650274cf
BambooShi <110565810+BambooShi@users.noreply.github.com>
2024-04-05 20:49:12 -0400

    added styff

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///login.db' #this set up the pa
 app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes = 10) # keep the session on for 10 min
 db = SQLAlchemy(app) 
 
+assignments = ['assignment_1', 'assignment_2', 'assignment_3', 'midterm', 'final']
 
 class Person(db.Model): # create a table using sqlalchemy
     __tablename__ = 'Person'
@@ -24,7 +25,8 @@ class Person(db.Model): # create a table using sqlalchemy
     
     def __init__(self, utorid, name, email, password, role, **kwargs):
         super().__init__(utorid=utorid, name=name, email=email, password=password, role=role, **kwargs)
-        self.grades = Grades(utorid=utorid)
+        if role == 'student':
+            self.grades = Grades(utorid=utorid)
 
     def __repr__(self): # string representation of the table
         return f"Person('{self.utorid}', '{self.name}', '{self.role})"
@@ -264,26 +266,31 @@ def query_grades(utorid):
 # helps instructors change/add a student's grade for an assignment 
 def manage():
     if "user" in session:
-        role = get_role()
         if request.method == 'GET':
-            remark_requests = query_remarks()
-            return render_template('manage.html', role=role, remark_requests=remark_requests)
+            role = get_role()
+            return render_template('manage.html', role=role, assignments=assignments, grades=Grades.query.all())
         else: # render this if the method is POST
             grade_details = (
-                request.form['Utorid'],
-                request.form['Assignment'],
-                request.form['Grade']
+                request.form['student_utorid'],
+                request.form['assignment_code'],
+                request.form['mark_update']
             )
             add_grades(grade_details)
             flash("Student's grade changed successfully!")
-            return render_template('manage.html', role=role)
+            role = get_role()
+            return render_template('manage.html', role=role, assignments=assignments, grades=Grades.query.all())
     return "Please login to view this page!"
 
 # change a student's grade for a specific assignment (into the db)    
 def add_grades(grade_details):
-    student = Grades.query.filter_by(utorid = grade_details[0])
-    setattr(student, grade_details[1], grade_details[2])
-    db.session.commit()
+    try:
+        student = Grades.query.filter_by(utorid = grade_details[0]).first()
+        if student:
+            setattr(student, grade_details[1], grade_details[2])
+            db.session.commit()
+    except Exception as e:
+        db.session.rollback()
+        print(e)
 
 def query_remarks():
     query_remark = Remark.query.all()
